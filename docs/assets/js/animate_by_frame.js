6fbcf177737d3c7450f1636dae87f99c76a1e227
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-07-19 23:58:33 -0700

    fix typo

--- docs/assets/js/animate_by_frame.js
+++ docs/assets/js/animate_by_frame.js
@@ -8,7 +8,7 @@ const observer = new IntersectionObserver(toggleVisibility);
 const animateUpElements = document.querySelectorAll('.up-show-on-scroll');
 const animateDownElements = document.querySelectorAll('.down-show-on-scroll');
 const animateRightElements = document.querySelectorAll('.right-show-on-scroll');
-const animateLeftElements = document.querySelectorAll('left-show-on-scroll');
+const animateLeftElements = document.querySelectorAll('.left-show-on-scroll');
 
 animateUpElements.forEach((element) => {
   observer.observe(element);
