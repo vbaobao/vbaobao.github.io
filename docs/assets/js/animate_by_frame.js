51a63d8c65a69883c0a41b5164c6c8829e53a536
vbaobao <vbao.main@gmail.com>
2021-07-22 18:35:07 -0700

    divide animations between toggle and one time use

--- docs/assets/js/animate_by_frame.js
+++ docs/assets/js/animate_by_frame.js
@@ -4,13 +4,34 @@ const toggleVisibility = (elements) => {
   });
 };
 
-const observer = new IntersectionObserver(toggleVisibility);
+const setVisible = (elements) => {
+  elements.forEach((element) => {
+    element.target.classList.add('is-visible');
+  });
+};
+
+const observerToggle = new IntersectionObserver(toggleVisibility);
+const observerSet = new IntersectionObserver(setVisible);
 const animateUpElements = document.querySelectorAll('.up-show-on-scroll');
 const animateDownElements = document.querySelectorAll('.down-show-on-scroll');
 const animateRightElements = document.querySelectorAll('.right-show-on-scroll');
 const animateLeftElements = document.querySelectorAll('.left-show-on-scroll');
-const animateElements = [...animateUpElements, ...animateDownElements, ...animateRightElements, ...animateLeftElements];
+const animateToggledElements = [...animateDownElements, ...animateLeftElements];
 
-animateElements.forEach((element) => {
-  observer.observe(element);
-});
+setTimeout(() => {
+  animateRightElements.forEach((element) => {
+    observerSet.observe(element);
+  })
+}, 500);
+
+setTimeout(() => {
+  animateUpElements.forEach((element) => {
+    observerSet.observe(element);
+  })
+}, 1500);
+
+setTimeout(() => {
+  animateToggledElements.forEach((element) => {
+    observerToggle.observe(element);
+  });
+}, 3000);
