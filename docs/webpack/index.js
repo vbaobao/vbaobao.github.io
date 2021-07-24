345f8f57bb073978ba0a9275413dd9b511d50cac
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-07-23 23:47:22 -0700

    render projects components

--- docs/webpack/index.js
+++ docs/webpack/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProjectCard from './components/ProjectCard';
+import Projects from '../assets/js/Projects.js';
+
+ReactDOM.render(<ProjectCard data={Projects} />, document.getElementById('projects'));
