336c3b43d6fbb6d6c53e88767d6e89e3dc41df16
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-07-28 19:22:15 -0700

    modify Projects component to map a list of Card components

--- docs/webpack/index.js
+++ docs/webpack/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProjectCard from './components/ProjectCard';
-import Projects from '../assets/js/Projects.js';
+import Card from './components/Card';
+import ProjectData from '../assets/js/Projects.js';
 
-ReactDOM.render(<ProjectCard data={Projects} />, document.getElementById('projects'));
+function Projects ({ data }) {
+  const cards = data.map((project) => <Card data={project} key={project.title.toString(2)} />);
+
+  return <ul>{cards}</ul>;
+};
+
+ReactDOM.render(<Projects data={ProjectData} />, document.getElementById('projects'));
