2759c7d77923165dbd6b1486376f5da6b7647767
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-07-29 23:39:09 -0700

    write email modal component

---
+++ docs/webpack/components/EmailModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmailModal = () => {
+  return (
+    <div className="modal-container">
+      <button className="click-to-close">close</button>
+      <p>contact<span>[at]</span>vbao<span>[dot]</span>info</p>
+    </div>
+  );
+};
+
+export default EmailModal;
