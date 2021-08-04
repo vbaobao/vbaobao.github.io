0538d377291422a1eb6f606616d16c8676f444ef
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-08-03 20:42:03 -0700

    add render of timeline

--- docs/webpack/index.js
+++ docs/webpack/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card';
 import EmailModal from './components/EmailModal';
+import Timeline from './components/Timeline';
 import ProjectData from '../assets/js/Projects.js';
+import HistoryData from '../assets/js/History.js';
 
 function Projects ({ data }) {
   const cards = data.map((project) => <Card data={project} key={project.title.toString(2)} />);
@@ -13,3 +15,5 @@ function Projects ({ data }) {
 ReactDOM.render(<Projects data={ProjectData} />, document.getElementById('projects'));
 
 ReactDOM.render(<EmailModal/>, document.getElementById('email-modal'));
+
+ReactDOM.render(<Timeline data={HistoryData}/>, document.getElementById('timeline'));
