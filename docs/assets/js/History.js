3b8c9b9e3f721ef5272355ba3ff8d6b37f00a26d
Virginia Bao <53163768+vbaobao@users.noreply.github.com>
2021-08-03 20:41:31 -0700

    init

---
+++ docs/assets/js/History.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    years: "2021 - present",
+    title: "Operations Project Coordinator",
+    company: "Adecco, on-site at Google",
+    description: "Incubated new processes to scale and streamline fraud investigation and resolution. Organized and codified insights from high priority fraud and abuse cases, including data standardization.",
+  },
+  {
+    years: "2020 - present",
+    title: "Web Developer",
+    company: "US Best Transport Inc.",
+    description: "Developed features within the internal company application that handles geohash tracking for each clientele, invoicing, expenses, and overall profit margin. Introduced prototypes of workflow features within the app, applying agile development methodologies and service oriented architecture.",
+  },
+  {
+    years: "2020 - 2021",
+    title: "Scaled Abuse Operations Data Analyst",
+    company: "Accenture Flex, on-site at Google",
+    description: "Created an efficient documentation process that reduced the workflow process from 30 minutes to 1 minute, reducing errors caused by data duplication by 80% through the integration of automation within the tracking of abuse networks. Identified rising abuse vectors and patterns and established standardized documentation to pinpoint them with efficiency. Introduced archiving and formatting to reduce documentation lag from bulk, e.g. from 72+ pages to 12 pages.",
+  },
+  {
+    years: "2017 - 2020",
+    title: "Internal Operations QA Analyst",
+    company: "VACO, on-site at Google",
+    description: "Constructed the foundations of WBR with clients and overseas teams. Established the workflow for project data collection, storage, analysis, and presentation creation. Streamlined report workflow by creating an analytics dashboard: automated graph generation, data calculation, and removed Excel/Google Sheets training requirements for report creations.",
+  },
+];
